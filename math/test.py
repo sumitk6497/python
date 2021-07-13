d722e41bbd46e8cbd7955ed2ec4f9bf39c58e43a
Sumit Kumar <sumitkn.pine@gmail.com>
2021-07-13 21:02:33 +0530

    Math

---
+++ math/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import mymath
+print("Sum = ",mymath.add(4,8))
+print("Sub = ",mymath.sub(14,8))
+print("Multiplication = ",mymath.mul(4,8))
+print("Division = ",mymath.div(4,8))
+print("Modulous = ",mymath.mod(4,8))
