79d7ac03fe523660286b21ce4a43216f93cce850
Sumit Kumar <sumitkn.pine@gmail.com>
2021-07-16 22:17:11 +0530

    input

---
+++ input.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+# Python3 program to get input from user
+
+# accepting integer from the user
+# the return type of input() function is string ,
+# so we need to convert the input to integer
+num1 = int(input("Enter num1 : "))
+num2 = int(input("Enter num2 : "))
+
+num3 = num1 * num2
+print("Product is : ", num3)
+
+num4 = num1 + num2
+print("Addition is : ", num4)
+
+num5 = num1 - num2
+print("Subtraction is : ", num5)
+
+num6 = num1 / num2
+print("Divison is : ", num6)
+
+num7 = num1 % num2
+print("Modulous is : ", num7)
+
