608c7bc93487b5a15eeff506679d57903c3c7769
Sumit Kumar <sumitkn.pine@gmail.com>
2021-07-16 22:16:15 +0530

    if-else

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+num = int(input("Please enter a no. : "))
+
+if(num < 20):
+    print("Entered no. is less than 20")
+elif(num > 50):
+    print("Entered no. is greater than 50")
+else:
+    print("Entered no. is in between 20 and 50")
