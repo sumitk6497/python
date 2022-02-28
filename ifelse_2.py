2117f4c248de5d2cb977923ea7bde985547e6d00
Sumit Kumar <sumitkn.pine@gmail.com>
2022-02-28 18:42:16 +0530

    if-else : 3 conditions in one line

---
+++ ifelse_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+a = int(input("Please enter 1st no. : "))
+b = int(input("Please enter 2nd no. : "))
+
+print("A > B") if a > b else print("A = B") if a == b else print("A < B") 
