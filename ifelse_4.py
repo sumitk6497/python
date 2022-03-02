c6f115bfde78453ce9ae022558a308b8fc54411f
Sumit Kumar <sumitkn.pine@gmail.com>
2022-03-02 20:05:00 +0530

    if-else : or keyword

---
+++ ifelse_4.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+a = int(input("Please enter 1st no. : "))
+b = int(input("Please enter 2nd no. : "))
+c = int(input("Please enter 3rd no. : "))
+
+if a > b or c > a:
+  print("At least one of conditions are True...")
+else:
+  print("Both conditions are not True...")
