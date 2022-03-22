48edadb95af6108a6c81eb28ba12747aa6ddfcf8
Sumit Kumar <sumitkn.pine@gmail.com>
2022-03-22 20:26:06 +0530

    function defination and calling

---
+++ fun_4.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def my_fun(fname, mname, lname):
+  print("My string is " + fname)
+
+my_fun(fname="Sumit", mname="Kumar", lname="nimesh")
+my_fun(fname="Python", mname="Language", lname="scripting")
